d4900298a18ef0688babb20d2a53d50ef1b2e84b
kteska <kasia@teska.pl>
2020-12-15 11:36:43 +0100

    Added frontend

--- backend/models/appointment/factory.js
+++ backend/models/appointment/factory.js
@@ -1,20 +1,20 @@
 const Appointment = require('./appointment')
-class AppointmentError extends Error {}
+//class AppointmentError extends Error {}
 
 const AppointmentFactory = async ({ title, date, participants, location }) => {
 
-    if (!title) {
-        throw new AppointmentError("Missing title")
-    }
-    if (!date) {
-        throw new AppointmentError("Missing date")
-    }
-    if (!participants) {
-        throw new AppointmentError("Missing participants")
-    }
-    if (!location) {
-        throw new AppointmentError("Missing location")
-    }
+    // if (!title) {
+    //     throw new AppointmentError("Missing title")
+    // }
+    // if (!date) {
+    //     throw new AppointmentError("Missing date")
+    // }
+    // if (!participants) {
+    //     throw new AppointmentError("Missing participants")
+    // }
+    // if (!location) {
+    //     throw new AppointmentError("Missing location")
+    // }
 
     return Appointment({ title, date, participants, location });
 }
